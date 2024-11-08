b53570ca7f7c352613e06c06d668b5706eb64058
big-ass-is-catching-you <99054170+big-ass-is-catching-you@users.noreply.github.com>
2024-11-08 09:39:17 +0800

    Update hostloc.py
    use curl_cffi to instead requests library.

--- hostloc.py
+++ hostloc.py
@@ -1,13 +1,12 @@
-import requests
+from curl_cffi import requests
 import time
 import re
 from datetime import date
 username = ''  # 论坛账户
 password = ''  # 论坛密码
 login_url = "https://hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes"
-user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'}
 # 登录请求
-request = requests.post(login_url, data={"username": username, 'password': password}, headers = user_agent)
+request = requests.post(login_url, data={"username": username, 'password': password}, impersonate="chrome")
 print(f"{date.today()} 登录状态码: {request.status_code}")  # 打印登录状态码
 # 检查登录是否成功
 if request.status_code != 200:
@@ -15,9 +14,7 @@ if request.status_code != 200:
     exit()
 
 # 获取用户积分中心
-user_info = requests.get('https://hostloc.com/home.php?mod=spacecp&ac=credit', headers = user_agent, cookies=request.cookies).text
-# 打印返回的 HTML 以调试
-#print(user_info)
+user_info = requests.get('https://hostloc.com/home.php?mod=spacecp&ac=credit', impersonate="chrome", cookies=request.cookies).text
 
 # 尝试获取金钱信息
 match = re.search(r'金钱: </em>(\d+).+?</li>', user_info)
@@ -29,8 +26,8 @@ else:
     exit()
 
 # 正题，刷积分
-for i in range(20359, 20390):
-    request1 = requests.get(f'https://hostloc.com/space-uid-{i}.html', headers = user_agent, cookies=request.cookies)
+for i in range(50780, 50800):
+    request1 = requests.get(f'https://hostloc.com/space-uid-{i}.html', impersonate="chrome", cookies=request.cookies)
     user_title = re.search(r'<title>(.+?)全球主机交流论坛', request1.text)
     
     if user_title:
@@ -39,7 +36,7 @@ for i in range(20359, 20390):
         print(f"未找到用户 {i} 的空间标题。")
 
     time.sleep(6)  # 可选的延时
-    new_money_info = requests.get('https://hostloc.com/home.php?mod=spacecp&ac=credit', headers = user_agent, cookies=request.cookies).text
+    new_money_info = requests.get('https://hostloc.com/home.php?mod=spacecp&ac=credit', impersonate="chrome", cookies=request.cookies).text
     new_money_match = re.search(r'金钱: </em>(\d+).+?</li>', new_money_info)
     
     if new_money_match:
